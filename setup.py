6b85185e11e7ec8dd9ca2c3e2fb23dbc6aea6eb8
Thanh Bình <maithanhbinh13102003@gmail.com>
2025-03-22 22:41:57 +0700

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Generate AI Project",
+    version="0.0.0",
+    author="Mai Thanh Binh",
+    author_email="maithanhbinh13102003@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
