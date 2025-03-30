c0abf051ac78c584f4e83cde879c8016d5dbfad1
Thanh Bình <maithanhbinh13102003@gmail.com>
2025-03-30 16:49:39 +0700

    fix: uncomment conda activation commands in start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-# export PATH="$HOME/miniconda/bin:$PATH"
-# source $HOME/miniconda/bin/activate
-# conda activate medibot
+export PATH="$HOME/miniconda/bin:$PATH"
+source $HOME/miniconda/bin/activate
+conda activate medibot
 pip install flask
 python app.py  # Chạy ứng dụng của bạn
